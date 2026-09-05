979a74d9a3e796cd8a6f7e46472ab38c2e6d4df9
Raman <ramanshah1525@gmail.com>
2026-09-05 09:33:31 +0530

    Time: 0 ms (100%), Space: 8.4 MB (62.82%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0292-nim-game/0292-nim-game.c
@@ -0,0 +1,12 @@
+bool canWinNim(int n) {
+    if(n%4==0){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
