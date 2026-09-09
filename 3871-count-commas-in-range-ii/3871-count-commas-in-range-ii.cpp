9cfad1c0aa227a293ed1106748d71f1590efc1be
Raman <ramanshah1525@gmail.com>
2026-09-09 13:38:46 +0530

    Time: 0 ms (100%), Space: 9 MB (84.8%) - LeetHub - Time: O(log n), Space: O(1)

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -3,6 +3,9 @@ public:
     long long countCommas(long long n) {
         long long count=0;
         long long prod=1000;
+        if(n<1000){
+            return 0;
+        }
         while(n>=prod){
             count=count+(n-prod+1);
             prod=prod*1000;
