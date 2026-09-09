de029c90a93996fc87a30194092ce32dea1c6a9b
Raman <ramanshah1525@gmail.com>
2026-09-09 13:36:46 +0530

    Time: 2 ms (6.67%), Space: 8.9 MB (99.2%) - LeetHub - Time: O(log₁₀ n), Space: O(1)

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long count=0;
+        long long prod=1000;
+        while(n>=prod){
+            count=count+(n-prod+1);
+            prod=prod*1000;
+        }
+        return count;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
