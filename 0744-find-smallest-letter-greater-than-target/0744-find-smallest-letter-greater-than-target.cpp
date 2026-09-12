2653a89c4b93e597e28956c1a6dad5a499a1c4c1
Raman <ramanshah1525@gmail.com>
2026-09-12 21:53:57 +0530

    Time: 0 ms (100%), Space: 19.7 MB (89.2%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int start=0,end=letters.size()-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(letters[mid]<=target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        if(start==letters.size()){
+           return letters[0];
+        }
+        return letters[start];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
